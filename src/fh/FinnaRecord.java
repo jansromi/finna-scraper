3ce3a41d6b0635ffd3b4b61ed6c1080d32ab9ae8
jansromi <jansromi@student.jyu.fi>
2024-01-06 17:02:04 +0200

    Add FinnaRecord class with methods to retrieve record details

---
+++ src/fh/FinnaRecord.java
@@ -0,0 +1,73 @@
+package fh;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class FinnaRecord {
+    private String finnaId;
+    private String isbn;
+    private JSONObject record;
+
+    public FinnaRecord(JSONObject record, String isbn, String finnaId) {
+        this.record = record;
+        this.isbn = isbn;
+        this.finnaId = finnaId;
+    }
+
+    /**
+     * @return The record as a JSONObject
+     */
+    public JSONObject getRecord() {
+        return record;
+    }
+
+    /**
+     * @return Title of the record
+     */
+    public String getRecordTitle() {
+        return FinnaParser.parseRecordTitle(record);
+    }
+
+    /**
+     * @return Languages of the record
+     */
+    public List<String> getRecordLanguages() {
+        return FinnaParser.parseRecordLanguages(record);
+    }
+
+    /**
+     * @return Primary and secondary authors of the record
+     */
+    public List<String> getRecordAuthors() {
+        return FinnaParser.parseRecordAuthors(record);
+    }
+
+    /**
+     * @return Subjects of the record
+     */
+    public List<String> getRecordSubjects() {
+        return FinnaParser.parseRecordSubjects(record);
+    }
+
+    /**
+     * @return Publishers of the record
+     */
+    public List<String> getRecordPublishers() {
+        return FinnaParser.parseRecordPublishers(record);
+    }
+
+    /**
+     * @return Publication dates of the record
+     */
+    public List<String> getRecordPublicationDates() {
+        return FinnaParser.parseRecordPublicationDates(record);
+    }
+
+    /**
+     * @return YKL classes of the record
+     */
+    public List<String> getRecordYKLClasses() {
+        return FinnaParser.parseRecordYKLClasses(record);
+    }
+}
